404853b4ecb07af0feaf177f7a1b50c5aed96e9b
rahul <rahul2018me015abesit@gmail.com>
2022-07-29 17:37:12 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  makeStyles,
+  Container,
+  Typography,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+// import { useState } from "react";
+
+interface IFormInput {
+  email: string;
+  firstName: string;
+  password: string;
+}
+
+const schema = yup.object().shape({
+  email: yup.string().required("Email is a required").email(),
+  firstName: yup.string().required().min(2).max(25),
+  password: yup.string().required().min(8).max(120),
+});
+
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    textAlign: "center",
+    margin: theme.spacing(1, 0, 4),
+  },
+  submitButton: {
+    marginTop: theme.spacing(4),
+  },
+}));
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({
+    resolver: yupResolver(schema),
+  });
+
+  const { heading, submitButton } = useStyles();
+
+
+  const onSubmit = (data: IFormInput) => {
+    alert("you have submitted the form")
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container maxWidth="xs">
+      <Typography className={heading} variant="h3">
+        Sign Up Form
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <TextField
+          {...register("email")}
+          variant="outlined"
+          margin="normal"
+          label="Email"
+          helperText={errors.email?.message}
+          error={!!errors.email?.message}
+          fullWidth
+          required
+        />
+        <TextField
+          {...register("firstName")}
+          variant="outlined"
+          margin="normal"
+          label="First Name"
+          helperText={errors.firstName?.message}
+          error={!!errors.firstName?.message}
+          fullWidth
+        />
+        <TextField
+          {...register("password")}
+          variant="outlined"
+          margin="normal"
+          label="Password"
+          helperText={errors.password?.message}
+          error={!!errors.password?.message}
+          type="password"
+          fullWidth
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={submitButton}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Sign Up
+        </Button>
+        {/* {json && (
+          <>
+            <Typography variant="body1">
+              Below is the JSON that would normally get passed to the server
+              when a form gets submitted
+            </Typography>
+            <Typography variant="body2"></Typography>
+          </>
+        )} */}
+      </form>
+    </Container>
   );
 }
 
